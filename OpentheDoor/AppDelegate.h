138432344ae9a969611bb9c4a3a67b46a7bbc5e2
barara <barara@bararadeimac.lan>
2016-01-27 17:26:11 +0800

    Initial Commit

---
+++ OpentheDoor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpentheDoor
+//
+//  Created by barara on 16/1/27.
+//  Copyright © 2016年 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
